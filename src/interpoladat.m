af5b4913115190db510f626bcf89d3d8060c0d85
Bruno Bedo <bruno.bedo@usp.br>
2021-12-07 16:40:56 -0300

    atualizei a funcao de interpolacao

--- src/interpoladat.m
+++ src/interpoladat.m
@@ -1,12 +1,12 @@
-function [datinterp]=interpoladat(coord2d,npontos);
+function [datinterp]=interpoladat(data2d,nppoints)
 % Interpola os dados para um tamanho conhecido
 % This file is part of DataGoal Toolbox: 
 % 
 % Author:   Bruno Luiz Souza Bedo <bruno.bedo@usp.br> 
 
 x=1:size(coord2d,1);
-xx=linspace(1,size(coord2d,1),npontos);
-datinterp(:,1)=interp1(x,coord2d(:,1),xx);
-datinterp(:,2)=interp1(x,coord2d(:,2),xx);
+X=linspace(1,size(coord2d,1),nppoints);
+datinterp(:,1)=interp1(x,coord2d(:,1),X);
+datinterp(:,2)=interp1(x,coord2d(:,2),X);
 
 end
