b781892e3b1dc7ac9f531b98b6d188d7c172572f
brunobedo <brunosebdo@gmail.com>
2022-12-15 17:16:14 -0300

    Novos Campos cadastrados

--- datagoal_GUI_v1.m
+++ datagoal_GUI_v1.m
@@ -1502,7 +1502,7 @@ function GPSType_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
-GPSType = {' ','QStarz','Stats Sports','PlayerTek','Dvideo','WIMU'};
+GPSType = {' ','QStarz','Stats Sports','PlayerTek','Dvideo','WIMU','Vector S7'};
 set(hObject,'String',GPSType); 
 
 
